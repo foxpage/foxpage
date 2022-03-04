b3f911b42060b20255590932b5b2ce1661ff8f0a
yhdeng <yhdeng@trip.com>
2022-03-04 19:17:14 +0800

    perf: ⚡️ clear foxpage admin config profile default settting

--- packages/foxpage-admin/config.profile.js
+++ packages/foxpage-admin/config.profile.js
@@ -1,21 +1,20 @@
 module.exports = {
   dev: {
     env: 'dev',
-    foxpageApi: 'http://ibu-qa-opensource-164331726.ap-southeast-1.elb.amazonaws.com/foxpage-server/',
-    // foxpageApi: 'api.foxfamily.io/',
-    ssrApi: 'http://ssr.api.foxfamily.io/',
+    foxpageApi: 'http://127.0.0.1:50000',
+    ssrApi: '',
     slug: '',
   },
   fat: {
     env: 'fat',
-    foxpageApi: 'http://ibu-qa-opensource-164331726.ap-southeast-1.elb.amazonaws.com/foxpage-server/',
-    ssrApi: 'http://ssr.api.foxfamily.io/',
-    slug: 'foxpage-admin',
+    foxpageApi: '',
+    ssrApi: '',
+    slug: '',
   },
   prd: {
     env: 'prd',
-    foxpageApi: 'http://api.foxfamily.io/',
-    ssrApi: 'http://ssr.api.foxfamily.io/',
-    slug: 'foxpage-admin',
+    foxpageApi: '/',
+    ssrApi: '',
+    slug: '',
   },
 };
